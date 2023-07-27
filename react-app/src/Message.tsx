afbf4d1e29e6af257a2b52fde5294be2af81bf14
Arduino Di Giosia <me@erme2.com>
2023-07-27 11:31:35 +0100

    temp

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+// you could also use a class or a function here, but better to use a function
+
+// function name should use PascalCase
+function Message() {
+  // this is jsx code
+  return <h1>Message</h1>;
+}
+// you must export the function to be able to use it
+export default Message;
